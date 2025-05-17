01204ae97c25db549fe146b576ba97cfa87dc6eb
Ridge <ridge.telegrafi@icloud.com>
2025-05-17 15:49:02 -0400

    Added combineUsers function to merge user arrays and include formatted merge date

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+require('datejs');
+function combineUsers(...args) {
+  let combinedObject={
+    users: []
+  }
+  for(const userArray of args){
+    if (!Array.isArray(userArray)) {
+      throw new TypeError('All arguments to combineUsers must be arrays of usernames');
+    }
+    combinedObject.users.push(...userArray);
+  }
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
 
+  return combinedObject;
+}
 
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+}
